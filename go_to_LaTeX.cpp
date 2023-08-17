2fbf1f7146a1572d8d5d5cb83504d79ca48eadc4
Rustam <azimzadeh.rustam@gmail.com>
2023-08-18 01:19:01 +0400

    make some changes

--- go_to_LaTeX.cpp
+++ go_to_LaTeX.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// Удаление пробелов и переносов в исходнойстроке
+// Удаление пробелов и переносов в исходной строке
 std::string removeHyphensAndSpaces(const std::string& input)
 {
     std::string result = input;
@@ -19,15 +19,18 @@ int main()
     setlocale(LC_ALL, "ru");
 
     // Исходная строка
-    std::string str = "-1/2*        e^2*      F#_1^1{-1}*     F_1_2{1}*           I{2^-4}*K5^-1{2}";
+    std::string str = "-1/2*        e^2*      F#_1^1{-1}*     F_1_   2{1}*           I{2  ^-4}*K  5^-1{2}";
 
     std::string cleanedString = removeHyphensAndSpaces(str);
+    
+    std::vector<std::string> search_patterns   = {"(d_\\{[\\w-]+\\}\\{[\\w-]+\\})",
+                                                  "2"};
 
-    /*
-    std::vector<std::string> search_patterns =   {"(d_\\{[\\w-]+\\}\\{[\\w-]+\\})",                  "2"};
+    std::vector<std::string> LaTeX_expressions = {"{\\delta}^8_{argument2}\\left(argument1\\right)",
+                                                  "2"};
 
-    std::vector<std::string> LaTeX_expressions = {"{\\delta}^8_{argument2}\\left(argument1\\right)", "2"};
-    */
+    std::vector<std::string> warnings          = {"дельта функции",
+                                                  "2" };
 
     std::cout << "Исходная строка:\n" << str << "\n";
     std::cout << "Очищенная строка:\n" << cleanedString << "\n";
